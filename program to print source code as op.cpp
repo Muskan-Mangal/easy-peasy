30de9e0014739a412b9a951560ee97ec0f678fd4
the-imperor <58086449+the-imperor@users.noreply.github.com>
2020-10-02 18:20:07 +0530

    sourcecode as print

---
+++ program to print source code as op.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	freopen("Untitled1.cpp","r",stdin);
+	//i'm taking printing this whole file as o/p
+	char ch;
+	int count=0;
+//	while( cin>>ch && ch!= EOF)
+//		cout<<ch;
+
+ while( (ch=getchar())!= EOF)
+		cout<<ch;
+	fclose(stdin);
+}
+
