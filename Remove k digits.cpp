e1ad464e51c5dc22e90834ea159877fbe62fd212
the-imperor <58086449+the-imperor@users.noreply.github.com>
2020-10-02 20:18:11 +0530

    remove k digits

---
+++ Remove k digits.cpp
@@ -0,0 +1,20 @@
+//short soln:
+
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        if (num.length() <= k) return "0";
+        string ret;
+        for (auto d : num) {
+            while (k && ret.length() && ret.back() > d) {
+                ret.pop_back();
+                --k;
+            }
+            ret.push_back(d);
+        }
+        ret.resize(ret.length() - k);
+        int beg = 0;
+        while (beg < ret.length() - 1 && ret[beg] == '0') ++beg;
+        return ret.substr(beg, ret.length() - beg + 1);
+    }
+};
